e6ba22aabb786bd7d804e7e3a77de19a888cdae8
RudezY <rudyyamashiro@yahoo.com>
2022-10-19 00:39:55 -0700

    psuedo coded JS for basic layout

---
+++ Assets/index.js
@@ -0,0 +1,38 @@
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+var highScores = document.querySelector(".High-Score");
+
+var timer;
+var timerCount;
+var highScores;
+
+// Array use to create questions and answers on the screen
+
+// Array of questions the user will be asked
+
+// the initial function called when the page loads
+
+// The begin quiz function when the button is clicked
+
+// the end quiz function is called when either all questions are answered or time runs out
+
+// the SetTimer function starts and stops the timer and will trigger the total score calculation
+
+// the SetHighScore function calculates score when either all questions are answered or time runs out
+
+// Creates questions on the screen
+
+// updates the high scores on screen and sends high scores to client storage
+
+// this function is used by init to grab the high scores from storage
+
+// attach even listener to begin quiz button on click
+
+//call init so it loads when page is opened
+
+// reset button for high scores
+
+// event listener for reset button on click
+
+
+
